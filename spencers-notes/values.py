40fd70776d85ad4ab1356d45ae94a2ecfdb0f790
Spencer <spencerthomas2017@gmail.com>
2025-01-29 17:26:00 -0600

    adding notes

---
+++ spencers-notes/values.py
@@ -0,0 +1,47 @@
+# GAME/TURN-ONE//
+# PL1TH/CHRISTIAN/6/BOARDWALK/B//
+# INFO/TURN ONE, PLAYER ONE, THE THIMBLE PIECE, CHRISTIAN, ROLLED A 6, AND LANDED ON BOARDWALK, BOUGHT IT//
+
+# P 
+# L
+# 1
+
+# Player
+# what about TH?
+
+# https://github.com/gamescomputersplay/monopoly/blob/main/classes/board.py
+# https://github.com/intrepidcoder/monopoly/blob/master/classicedition.js
+
+# both repos use similar locations, same as decks
+
+# player deck
+
+# advance to given property -> dont collect money if pass go
+# advance to given property -> did you cross or advance to go? collect money if so
+# advance to most close property type -> if owned, pay owner twice of amount that rent is
+# advance to most close property type -> if owned, roll dice and pay owner ten times amount
+# bank gives dividend
+# get out of jail for free
+# go back 3 spaces
+# go to jail, dont pass go or collect money
+# pay general repairs for each property, pay -> each house 25 -> 200 each hotel
+# fined for speeding
+# building loan matures
+# are elected chairman and need to pay each player 50 
+
+
+
+# community deck
+
+# advance to go and collect 200
+# bank error colect 200
+# fee pay 50
+# get out of jail for free
+# go to jail, dont pass go or collect money
+# fund matures
+# something happened, pay 40 per house, 115 per hotel
+
+
+# look at properties next
+# remember that based on how many houses/hotels are on something, its cost rises
+# can it ever go down?
